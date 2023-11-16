c0cfb025be4475d9faf3a94dc5cde178a3677508
Peterson Bersanetti <74914733+petersonbersanetti@users.noreply.github.com>
2023-11-16 17:48:51 -0300

    Inicio do Projeto
    Implementando Web Project Admin Painel

---
+++ src/main/java/br/com/fakeuniversity/FakeUniversityApplication.java
@@ -0,0 +1,13 @@
+package br.com.fakeuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fakeuniversity/FakeUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fakeuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
